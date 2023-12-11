ff9a9e2e8e0639341d06ca5f9bb7eb9e269e9f8a
ShinEun9 <ses2201@naver.com>
2023-12-11 23:24:10 +0900

    ♻️ Refactor: PostCard 마크업 및 코드 스타일 변경

--- src/components/Common/PostCard/PostCard.jsx
+++ src/components/Common/PostCard/PostCard.jsx
@@ -1,20 +1,25 @@
 import React, { useMemo, useState } from 'react';
-import { useLocation, useNavigate, useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import styled from 'styled-components';
-import { UserInfo, IconButton } from 'components/Common';
+import { UserInfo, ImageCarousel, MoreButton } from 'components/Common';
 import { ReactComponent as HeartIcon } from 'assets/icons/icon-heart.svg';
 import { ReactComponent as CommentIcon } from 'assets/icons/icon-message-small.svg';
-import { ReactComponent as MoreIcon } from 'assets/icons/icon-more-small.svg';
 import { convertTime } from 'utils';
 import { likeAPI } from 'api/apis/like';
 
 const PostArticle = styled.article`
   position: relative;
   width: 358px;
+
+  & > button {
+    position: absolute;
+    top: ${({ isInProfile }) => (isInProfile ? '-40px' : '0px')};
+    right: 0px;
+  }
 `;
 
-const PostContent = styled.section`
-  margin: ${({ isProfile }) => (!isProfile ? '12px 0' : '0 0 12px 0')};
+const PostContentSection = styled.section`
+  margin: ${({ isInProfile }) => (isInProfile ? '0 0 12px 0' : '12px 0')};
   cursor: pointer;
   position: relative;
   overflow: hidden;
@@ -25,7 +30,8 @@ const PostContent = styled.section`
     line-height: 18px;
     margin-bottom: 12px;
     word-break: break-all;
-    &.post-preview {
+
+    .post-preview {
       overflow: hidden;
       white-space: normal;
       text-overflow: ellipsis;
@@ -44,36 +50,7 @@ const PostContent = styled.section`
   }
 `;
 
-const ImageCarousel = styled.ul`
-  display: flex;
-  transition: transform 0.3s ease;
-  transform: ${({ currentSlide }) => `translateX(-${currentSlide * 100}%)`};
-
-  li {
-    flex: 0 0 100%;
-  }
-`;
-
-const ImageCarouselButtons = styled.div`
-  position: absolute;
-  bottom: 10px;
-  display: flex;
-  justify-content: center;
-  width: 100%;
-`;
-
-const ImageCarouselButton = styled.button`
-  flex: 0 0 10px;
-  height: 10px;
-  margin: 0 5px;
-  padding: 0;
-  border-radius: 50%;
-  background-color: ${({ active }) =>
-    active ? 'var(--main-color)' : 'var(--sub-text-color)'};
-  cursor: pointer;
-`;
-
-const ContentInfo = styled.section`
+const ContentInfoSection = styled.section`
   display: flex;
   justify-content: space-between;
   align-items: center;
@@ -90,30 +67,33 @@ const ContentInfo = styled.section`
   }
 `;
 
+const InfoButtonWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 10px;
+
+  & > button {
+    font-size: 12px;
+    display: flex;
+    align-items: center;
+    gap: 5px;
+  }
+`;
+
 const StyledHeartIcon = styled(HeartIcon)`
   stroke: ${({ $liked }) =>
     $liked ? 'var(--main-color)' : 'var(--sub-text-color)'};
   fill: ${({ $liked }) => ($liked ? 'var(--main-color)' : 'transparent')};
 `;
 
-const InfoIcons = styled.div`
-  display: flex;
-  align-items: center;
-`;
-
-const IconText = styled.span`
-  font-size: 12px;
-  margin: 0 18px 0 6px;
-`;
-
-const MoreButton = styled.button`
-  position: absolute;
-  top: ${({ isProfile }) => (!isProfile ? '0px' : '-40px')};
-  right: 0px;
-  padding: 10px;
-`;
-
 export default function PostCard({ post, setPostId, setIsMenuOpen }) {
+  const navigate = useNavigate();
+  const isInProfile = useMemo(
+    () => location.pathname.includes('profile'),
+    [location.pathname],
+  );
+  const { post_id: postIdParams } = useParams();
+
   const {
     id,
     author,
@@ -129,16 +109,6 @@ export default function PostCard({ post, setPostId, setIsMenuOpen }) {
     liked: hearted,
     count: heartCount,
   });
-  const [currentSlide, setCurrentSlide] = useState(0);
-
-  const navigate = useNavigate();
-  const pathname = useLocation().pathname;
-  const isProfile = pathname.includes('profile');
-  const { post_id: postIdParams } = useParams();
-
-  const imageList = useMemo(() => {
-    return image.split(',');
-  }, [image]);
 
   const handleLike = async () => {
     const { liked, count } = await likeAPI(id, likeInfo.liked);
@@ -148,79 +118,49 @@ export default function PostCard({ post, setPostId, setIsMenuOpen }) {
     });
   };
 
-  const handleSlideChange = (index) => {
-    setCurrentSlide(index);
-  };
-
   const handleMenu = () => {
     setIsMenuOpen(true);
     setPostId(id);
   };
 
   return (
-    <PostArticle>
+    <PostArticle isInProfile={isInProfile}>
       <h3 className="a11y-hidden">포스트</h3>
-      {!isProfile ? <UserInfo user={author} /> : null}
-      <PostContent
-        isProfile={isProfile}
+
+      {!isInProfile && <UserInfo user={author} />}
+      {<MoreButton onClick={handleMenu}>더보기버튼</MoreButton>}
+
+      <PostContentSection
         onClick={() => {
           navigate(`/post/${id}`);
         }}
+        isInProfile={isInProfile}
       >
         <h4 className="a11y-hidden">포스트 내용</h4>
-        <p className={!postIdParams ? 'post-preview' : null}>{content}</p>
-        <ImageCarousel currentSlide={currentSlide}>
-          {image &&
-            imageList.map((image, index) => {
-              return (
-                <li key={id + index}>
-                  <img src={image} alt={`이미지 ${index + 1}`} />
-                </li>
-              );
-            })}
-        </ImageCarousel>
-        {imageList.length > 1 && (
-          <ImageCarouselButtons>
-            {imageList.map((_, index) => (
-              <ImageCarouselButton
-                key={index}
-                active={index === currentSlide}
-                onClick={(e) => {
-                  e.stopPropagation();
-                  handleSlideChange(index);
-                }}
-                aria-label={`이미지 ${index + 1}`}
-              />
-            ))}
-          </ImageCarouselButtons>
-        )}
-      </PostContent>
-      <ContentInfo>
+        <p className={postIdParams ? '' : 'post-preview'}>{content}</p>
+        <ImageCarousel image={image} postId={id} />
+      </PostContentSection>
+      <ContentInfoSection>
         <h4 className="a11y-hidden"> 좋아요 갯수, 댓글 갯수 및 게시글 날짜</h4>
-        <InfoIcons>
-          <IconButton>
+        <InfoButtonWrapper>
+          <button className={'button'} type="button" onClick={handleLike}>
             <span className="a11y-hidden">좋아요 버튼</span>
             <StyledHeartIcon onClick={handleLike} $liked={likeInfo.liked} />
-          </IconButton>
-          <IconText>{likeInfo.count}</IconText>
-          <div
+            <span className="contents">{likeInfo.count}</span>
+          </button>
+          <button
+            type="button"
             onClick={() => {
               navigate(`/post/${id}`);
             }}
           >
-            <IconButton>
-              <span className="a11y-hidden">상세 글 정보보기 버튼</span>
-              <CommentIcon fill="white" stroke="var(--sub-text-color)" />
-            </IconButton>
-            <IconText>{commentCount}</IconText>
-          </div>
-        </InfoIcons>
+            <span className="a11y-hidden">상세 글 정보보기 버튼</span>
+            <CommentIcon fill="white" stroke="var(--sub-text-color)" />
+            <span className="contents">{commentCount}</span>
+          </button>
+        </InfoButtonWrapper>
         <time dateTime={createdAt}>{convertTime(createdAt)}</time>
-      </ContentInfo>
-      <MoreButton type="button" isProfile={isProfile} onClick={handleMenu}>
-        <span className="a11y-hidden">더보기 버튼</span>
-        <MoreIcon />
-      </MoreButton>
+      </ContentInfoSection>
     </PostArticle>
   );
 }
