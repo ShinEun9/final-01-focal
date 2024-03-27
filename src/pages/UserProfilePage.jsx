0c5e9f5d29e0a9245f78abf22260e9380d3658a9
ShinEun9 <ses2201@naver.com>
2024-03-27 17:47:13 +0900

    Refactor: UserProfilePage useProfileDataFetch 훅 사용하여 로직 변경

--- src/pages/UserProfilePage.jsx
+++ src/pages/UserProfilePage.jsx
@@ -1,17 +1,18 @@
-import { useState, useEffect, useRef } from 'react';
-import { useParams } from 'react-router-dom';
+import { useRef } from 'react';
 import styled from 'styled-components';
+import { useParams } from 'react-router-dom';
 import { Header, NavBar, Loading } from 'layouts';
 import { ProfileInfo, ProfileProducts, ProfilePosts } from 'components/Profile';
-import { profileAPI } from 'api/apis/profile';
+import { useProfileDataFetch } from 'hooks';
 
 const Main = styled.main`
   width: 100%;
-  max-height: calc(100vh - 108px);
+  height: calc(100vh - 108px);
   overflow-y: auto;
   display: flex;
   flex-direction: column;
   align-items: center;
+  justify-content: ${({ isLoading }) => (isLoading ? 'center' : 'flex-start')};
   min-width: 380px;
   margin-top: 48px;
   background-color: #f2f2f2;
@@ -19,51 +20,46 @@ const Main = styled.main`
 `;
 
 export default function UserProfilePage() {
-  const [isUserLoading, setIsUserLoading] = useState(true);
-  const [isProductLoading, setIsProductLoading] = useState(true);
-  const [isPostLoading, setIsPostLoading] = useState(true);
-  const [userData, setUserData] = useState('');
-  const { _id } = useParams();
-
   const elementRef = useRef(null);
 
-  useEffect(() => {
-    const fetchUserData = async () => {
-      const res = await profileAPI(_id);
-      setUserData(res);
-      setIsUserLoading(false);
-    };
-    fetchUserData();
-  }, []);
+  const {
+    profileInfo,
+    productList,
+    postList,
+    isLoading,
+    isUserIsSameWithLoginUser,
+  } = useProfileDataFetch();
+
+  const { account_name } = useParams();
 
   return (
     <>
-      {(isUserLoading || isProductLoading || isPostLoading) && <Loading />}
-      {!(isUserLoading && isProductLoading && isPostLoading) && (
-        <>
-          <Header type="basic" />
-          <Main ref={elementRef}>
-            <h1 className="a11y-hidden">
-              ${userData.accountname}의 프로필 페이지
-            </h1>
-            {userData && (
-              <>
-                <ProfileInfo userInfo={userData} />
-                <ProfileProducts
-                  accountname={userData.accountname}
-                  setIsProductLoading={setIsProductLoading}
-                />
-                <ProfilePosts
-                  elementRef={elementRef}
-                  accountname={userData.accountname}
-                  setIsPostLoading={setIsPostLoading}
-                />
-              </>
-            )}
-          </Main>
-          <NavBar />
-        </>
-      )}
+      <>
+        <Header type="basic" />
+        <Main ref={elementRef} isLoading={isLoading}>
+          <h1 className="a11y-hidden">${account_name}의 프로필 페이지</h1>
+          {isLoading ? (
+            <Loading />
+          ) : (
+            <>
+              <ProfileInfo
+                profileInfo={profileInfo}
+                isUserIsSameWithLoginUser={isUserIsSameWithLoginUser}
+              />
+              <ProfileProducts
+                productList={productList}
+                isUserIsSameWithLoginUser={isUserIsSameWithLoginUser}
+              />
+              <ProfilePosts
+                elementRef={elementRef}
+                postList={postList}
+                isUserIsSameWithLoginUser={isUserIsSameWithLoginUser}
+              />
+            </>
+          )}
+        </Main>
+        <NavBar />
+      </>
     </>
   );
 }
