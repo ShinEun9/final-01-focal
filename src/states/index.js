04236317dffe27b2fefc308571bddccf3cb06b01
ShinEun9 <ses2201@naver.com>
2024-03-27 13:51:42 +0900

    Add: loadingState atomFamily, selector 추가

--- src/states/index.js
+++ src/states/index.js
@@ -1,4 +1,5 @@
 import loginState from './LoginState';
 import pathState from './PathState';
+import { loadingStateFamily, profileLoadingState } from './ProfileLoadingState';
 
-export { loginState, pathState };
+export { loginState, pathState, loadingStateFamily, profileLoadingState };
