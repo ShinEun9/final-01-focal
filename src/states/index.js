d994bd1e4dca7bce9b8fd99ef6dbfddf760de9a4
ShinEun9 <ses2201@naver.com>
2024-03-27 16:44:44 +0900

    Remove: ProfileLoadingState 전역상태 삭제

--- src/states/index.js
+++ src/states/index.js
@@ -1,5 +1,4 @@
 import loginState from './LoginState';
 import pathState from './PathState';
-import { loadingStateFamily, profileLoadingState } from './ProfileLoadingState';
 
-export { loginState, pathState, loadingStateFamily, profileLoadingState };
+export { loginState, pathState };
