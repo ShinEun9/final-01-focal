5bd5e0a7d826fb6a215abca427efda725edb0ec0
ShinEun9 <ses2201@naver.com>
2024-03-27 18:13:48 +0900

    Add: API 함수 index파일 추가

---
+++ src/api/apis/index.js
@@ -0,0 +1,76 @@
+import {
+  getCommentListAPI,
+  createPostCommentAPI,
+  deleteCommentAPI,
+  reportCommentAPI,
+} from './comment';
+
+import { followAPI, unfollowAPI, followerAPI, followingAPI } from './follow';
+
+import { getImageSrcAPI, getMultiImageSrcAPI } from './image';
+
+import { likeAPI } from './like';
+
+import {
+  getPostsAPI,
+  getPostAPI,
+  createPostAPI,
+  editPostAPI,
+  deletePostAPI,
+  reportPostAPI,
+  getUserPostsAPI,
+} from './post';
+
+import {
+  getProductsAPI,
+  getProductAPI,
+  postProudctAPI,
+  editProductAPI,
+  deleteProductAPI,
+} from './product';
+
+import { getProfileAPI } from './profile';
+
+import {
+  loginAPI,
+  emailValidAPI,
+  accountnameValidAPI,
+  signupAPI,
+  getMyInfoAPI,
+  editMyInfoAPI,
+  searchUserAPI,
+} from './user';
+
+export {
+  getCommentListAPI,
+  createPostCommentAPI,
+  deleteCommentAPI,
+  reportCommentAPI,
+  followAPI,
+  unfollowAPI,
+  followerAPI,
+  followingAPI,
+  getImageSrcAPI,
+  getMultiImageSrcAPI,
+  likeAPI,
+  getPostsAPI,
+  getPostAPI,
+  createPostAPI,
+  editPostAPI,
+  deletePostAPI,
+  reportPostAPI,
+  getUserPostsAPI,
+  getProductsAPI,
+  getProductAPI,
+  postProudctAPI,
+  editProductAPI,
+  deleteProductAPI,
+  getProfileAPI,
+  loginAPI,
+  emailValidAPI,
+  accountnameValidAPI,
+  signupAPI,
+  getMyInfoAPI,
+  editMyInfoAPI,
+  searchUserAPI,
+};
