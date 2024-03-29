cf026025e1abcfc655f0d1347acac7c793274ba5
ShinEun9 <ses2201@naver.com>
2024-03-29 18:08:14 +0900

    Feat: SearchPage Loading 추가

--- src/pages/SearchPage.jsx
+++ src/pages/SearchPage.jsx
@@ -2,7 +2,7 @@ import React, { useCallback, useEffect, useState } from 'react';
 import AutoSizer from 'react-virtualized-auto-sizer';
 import { FixedSizeList } from 'react-window';
 import styled from 'styled-components';
-import { Header, NavBar } from 'layouts';
+import { Header, Loading, NavBar } from 'layouts';
 import { UserSearchListItem } from 'components/Search';
 import { useDebounce } from 'hooks';
 import { searchUserAPI } from 'api/apis';
@@ -14,21 +14,31 @@ const Main = styled.main`
 
   & > section {
     height: 100%;
+    display: flex;
+    justify-content: ${({ isLoading }) =>
+      String(isLoading) === 'true' ? 'center' : 'flex-start'};
+    align-items: ${({ isLoading }) =>
+      String(isLoading) === 'true' ? 'center' : 'flex-start'};
   }
 `;
 
 export default function SearchPage() {
   const [inputValue, setInputValue] = useState('');
   const [users, setUsers] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
   const debouncedValue = useDebounce(inputValue);
 
   const getData = useCallback(async () => {
+    setIsLoading(true);
+
     if (inputValue) {
       const { data } = await searchUserAPI(inputValue);
       setUsers(data);
     } else {
       setUsers([]);
     }
+
+    setIsLoading(false);
   }, [debouncedValue]);
 
   useEffect(() => {
@@ -43,28 +53,32 @@ export default function SearchPage() {
           setInputValue(e.target.value);
         }}
       />
-      <Main>
+      <Main isLoading={isLoading}>
         <section>
           <h2 className="a11y-hidden">검색 리스트 결과</h2>
-          <AutoSizer style={{ height: '100%', width: '100%' }}>
-            {({ height }) => (
-              <FixedSizeList
-                height={height}
-                itemCount={users.length}
-                itemSize={65}
-                width={'100%'}
-              >
-                {({ index, style }) => (
-                  <UserSearchListItem
-                    key={users[index]._id}
-                    user={users[index]}
-                    searchQuery={inputValue}
-                    style={style}
-                  />
-                )}
-              </FixedSizeList>
-            )}
-          </AutoSizer>
+          {isLoading && <Loading />}
+
+          {!isLoading && (
+            <AutoSizer style={{ height: '100%', width: '100%' }}>
+              {({ height }) => (
+                <FixedSizeList
+                  height={height}
+                  itemCount={users.length}
+                  itemSize={65}
+                  width={'100%'}
+                >
+                  {({ index, style }) => (
+                    <UserSearchListItem
+                      key={users[index]._id}
+                      user={users[index]}
+                      searchQuery={inputValue}
+                      style={style}
+                    />
+                  )}
+                </FixedSizeList>
+              )}
+            </AutoSizer>
+          )}
         </section>
       </Main>
 
